67636282fb71e828ff8a0ec73e6a13c692030712
eumesmo <@git>
2023-09-18 17:44:38 -0300

    NEWTHEMES

---
+++ packages/sx05re/emuelec-emulationstation/themes/LZ-PSX-STATIONS-2023/package.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="LZ-PSX-STATIONS-2023"
+PKG_VERSION="e36de58d902f06409894687f166f3278ba6c68c2"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/coreelecemuelec/LZ-PSX-STATIONS-2023"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emuelec"
+PKG_SHORTDESC="LZ-PSX-STATIONS-2023"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  : not
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/config/emulationstation/themes/LZ-PSX-STATIONS-2023 
+    cp -r * $INSTALL/usr/config/emulationstation/themes/LZ-PSX-STATIONS-2023 
+}
